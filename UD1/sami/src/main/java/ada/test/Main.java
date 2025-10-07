fb19c6b62bd67d4a846c4989346e89e1b8df4313
SamiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2025-10-07 12:58:54 +0200

    Commit desde casa

---
+++ UD1/sami/src/main/java/ada/test/Main.java
@@ -0,0 +1,7 @@
+package ada.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola!, este proyecto ha sido creado para testear el funcionamiento de GitHub integrado en VS Code.");
+    }
+}
